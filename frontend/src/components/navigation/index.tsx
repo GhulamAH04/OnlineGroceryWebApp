a51430be839037588a4f3238ad1e045cf96fcf6a
Nahalil <nahalilmuchtar2@gmail.com>
2025-06-15 00:09:25 +0800

    Navbar done

---
+++ frontend/src/components/navigation/index.tsx
@@ -0,0 +1,13 @@
+import SmallOne from "./components/small-one";
+import Middle from "./components/middle";
+import NavLinks from "./components/nav-links";
+
+export default function Navbar() {
+  return (
+    <>
+      <SmallOne/>
+      <Middle/>
+      <NavLinks/>
+    </>
+  );
+}
