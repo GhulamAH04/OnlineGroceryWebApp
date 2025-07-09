8aac9587cbcd6457c84197922733e1f4b36c780e
Nahalil <nahalilmuchtar2@gmail.com>
2025-07-09 10:39:04 +0800

    feat1/fe/Protected Route (Only Profile For Now)

---
+++ frontend/src/middleware.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { cookies } from "next/headers";
+
+export default async function middleware(req: NextRequest) {
+  try {
+    const cookieStore = await cookies();
+
+    const protectedRoute =
+      req.nextUrl.pathname === "/profile" || "/profile/:path*";
+
+    const access_token = cookieStore.get("access_token")?.value || "";
+
+    if (protectedRoute && !access_token) {
+      return NextResponse.redirect(new URL("/login", req.nextUrl));
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.redirect(new URL("/login", req.nextUrl));
+  }
+}
+
+export const config = {
+  matcher: ["/profile/:path*"], // Apply middleware to all paths under /dashboard
+};
